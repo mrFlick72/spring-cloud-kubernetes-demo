012bb59265585dd97f81a539a7bfb4e4431bc95c
mrFlick72 <valerio.vaudi@gmail.com>
2019-04-13 23:21:59 +0200

    kubernetes DNS service discovery

--- hello-service/src/main/java/it/valeriovaudi/helloservice/HelloServiceApplication.java
+++ hello-service/src/main/java/it/valeriovaudi/helloservice/HelloServiceApplication.java
@@ -1,9 +1,9 @@
 package it.valeriovaudi.helloservice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
@@ -20,7 +20,6 @@ import java.util.UUID;
 import static java.lang.String.*;
 import static org.springframework.web.reactive.function.server.ServerResponse.*;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class HelloServiceApplication {
 
@@ -29,31 +28,32 @@ public class HelloServiceApplication {
     }
 
     @Bean
-    @LoadBalanced
     public WebClient webClient() {
-        return WebClient.builder()
-                .build();
+        return WebClient.builder().build();
     }
 }
 
+@Slf4j
 @Service
 class HelloService {
 
     private final String instanceID = UUID.randomUUID().toString();
     private final String template = "Hello %s from service instance %s the special message for you to day is %s";
 
+    private final String helloServiceUri;
     private final WebClient webClient;
 
-    HelloService(WebClient webClient) {
+    HelloService(@Value("${hello-service-uri:}") String helloServiceUri, WebClient webClient) {
+        this.helloServiceUri = helloServiceUri;
         this.webClient = webClient;
     }
 
-
     Mono<String> sayHello(String name) {
+        System.out.println("helloServiceUri: " + helloServiceUri);
         return webClient.get()
-                .uri("http://message-service/message/random")
-                .exchange()
-                .flatMap(clientResponse -> clientResponse.bodyToMono(String.class))
+                .uri(helloServiceUri)
+                .retrieve()
+                .bodyToMono(String.class)
                 .flatMap(message -> Mono.just(format(template, name, instanceID, message)));
     }
 }
