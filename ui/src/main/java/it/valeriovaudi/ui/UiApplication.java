859d724160d889b4c02fdb345e63da3af8d8ce0a
mrFlick72 <valerio.vaudi@gmail.com>
2019-05-05 15:41:03 +0200

    spring security and session

--- ui/src/main/java/it/valeriovaudi/ui/UiApplication.java
+++ ui/src/main/java/it/valeriovaudi/ui/UiApplication.java
@@ -3,6 +3,15 @@ package it.valeriovaudi.ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static java.util.Arrays.asList;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -12,4 +21,36 @@ public class UiApplication {
         SpringApplication.run(UiApplication.class, args);
     }
 
+}
+
+@EnableWebFluxSecurity
+class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange()
+                .pathMatchers("/index.html").hasRole("USER")
+                .pathMatchers("/messages.html").hasRole("ADMIN")
+                .anyExchange().permitAll()
+                .and().formLogin()
+                .and().logout()
+                .and().build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("secret")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("secret")
+                .roles("USER")
+                .build();
+
+        return new MapReactiveUserDetailsService(asList(admin, user));
+    }
 }
