ade44a857c52149f3c75d791ac5ea5ee6daa7fbd
mrflick72 <valerio.vaudi@gmail.com>
2022-09-30 19:43:51 +0200

    [skip ci]

---
+++ kubernetes/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+kubectl apply -f namespace.yml
+
+kubectl apply -f mongo.yml
+kubectl apply -f redis.yml
+
+kubectl apply -f message-service.yml
+kubectl apply -f hello-service.yml
+kubectl apply -f ui-interface.yml
