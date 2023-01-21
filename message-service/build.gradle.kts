71086d0fa26911da8cb14e1893e62a092ac19ec0
mrflick72 <valerio.vaudi@gmail.com>
2023-01-21 14:50:11 +0100

    put bootstrap logic

--- message-service/build.gradle.kts
+++ message-service/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.cloud:spring-cloud-starter-kubernetes-client-all")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
