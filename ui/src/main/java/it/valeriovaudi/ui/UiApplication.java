bd6e6847c8c61e54da4ccb9f0208feac05673a1d
mrflick72 <valerio.vaudi@gmail.com>
2023-06-10 16:01:54 +0200

    proper configuration for roles

--- ui/src/main/java/it/valeriovaudi/ui/UiApplication.java
+++ ui/src/main/java/it/valeriovaudi/ui/UiApplication.java
@@ -7,7 +7,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class UiApplication {
@@ -16,6 +27,37 @@ public class UiApplication {
         SpringApplication.run(UiApplication.class, args);
     }
 
+}
+
+class UserIntrospectorOidcUserService implements OAuth2UserService<OidcUserRequest, OidcUser> {
+
+    private final OidcUserService delegate;
+
+    public UserIntrospectorOidcUserService(OidcUserService delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public OidcUser loadUser(OidcUserRequest userRequest) {
+        OidcUser oidcUser = delegate.loadUser(userRequest);
+        Collection<GrantedAuthority> mappedAuthorities = authoritiesFor(oidcUser);
+
+        return new DefaultOidcUser(mappedAuthorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
+    }
+
+
+    private Set<GrantedAuthority> authoritiesFor(OidcUser user) {
+        List<String> authorities = authoritiesFrom(user);
+        return authorities.stream()
+                .map(SimpleGrantedAuthority::new)
+                .map(authority -> new OidcUserAuthority(authority.getAuthority(), user.getIdToken(), user.getUserInfo()))
+                .collect(Collectors.toSet());
+    }
+
+    private List<String> authoritiesFrom(OidcUser oidcUser) {
+        List<String> authoritiesClaim = (List<String>) oidcUser.getClaimAsMap("realm_access").get("roles");
+        return Optional.ofNullable(authoritiesClaim).orElse(Collections.emptyList());
+    }
 }
 
 @Configuration(proxyBeanMethods = false)
@@ -34,15 +76,20 @@ class SecurityConfig {
         );
 
 
-        http.oauth2Login(loginConfigurer -> loginConfigurer.defaultSuccessUrl("/index"));
+        http.oauth2Login(configurer ->
+                configurer.defaultSuccessUrl("/index").userInfoEndpoint(config ->
+                        config.oidcUserService(
+                                new UserIntrospectorOidcUserService(new OidcUserService()))
+                ));
 
         http.authorizeHttpRequests(
                 auth ->
                         auth
-                                .requestMatchers("/index.html").hasRole("USER")
-                                .requestMatchers("/messages.html").hasRole("USER")
+                                .requestMatchers("/index.html").hasAuthority("USER")
+                                .requestMatchers("/messages.html").hasAuthority("USER")
                                 .anyRequest().permitAll()
         );
+
 
         return http.build();
     }
