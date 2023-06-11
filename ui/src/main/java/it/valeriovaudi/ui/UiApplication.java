cfcc6d60797b05ac904acfe5774e8eff45432658
mrflick72 <valerio.vaudi@gmail.com>
2023-06-11 11:42:19 +0200

    proper configuration for roles

--- ui/src/main/java/it/valeriovaudi/ui/UiApplication.java
+++ ui/src/main/java/it/valeriovaudi/ui/UiApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -15,7 +17,6 @@ import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -60,6 +61,7 @@ class UserIntrospectorOidcUserService implements OAuth2UserService<OidcUserReque
     }
 }
 
+@EnableWebSecurity
 @Configuration(proxyBeanMethods = false)
 class SecurityConfig {
 
